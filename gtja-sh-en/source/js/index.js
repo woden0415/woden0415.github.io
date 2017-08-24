529577d47af1260012c2892c756327ddbaeee10d
woden0415 <woden0415@163.com>
2017-08-24 17:25:38 +0800

    del gtja-sh-en

--- gtja-sh-en/source/js/index.js
+++
@@ -1,6 +0,0 @@
-/* 测试注释 */
-console.log('this is index.js')
-function add(a, b) {
-  console.log(a + b)
-}
-add(1, 2)
--- gtja-sh-en/target/js/components/banner.js
+++
@@ -1,7 +0,0 @@
-;$(function(){
-  $(".banner").hover(function(){
-    $(".funbtn a.left, .funbtn a.right").show();
-  },function(){
-    $(".funbtn a.left, .funbtn a.right").hide();
-  });
-});
